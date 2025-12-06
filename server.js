595303a0ca300893b61f5a0c774e53930cc73e15
gopika <b60723vpm1@gmail.com>
2025-12-06 22:27:23 +0530

    Fix correct server.js file for render

--- server.js
+++ server.js
@@ -45,7 +45,8 @@ app.delete("/delete/:id", async (req, res) => {
     await User.findByIdAndDelete(req.params.id);
     res.send({ message: "User Deleted" });
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
